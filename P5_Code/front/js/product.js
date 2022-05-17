ac35d600164778ea51ea0c2791669d9e1222eee0
Killian.Sb <TheKller29@Gmail.com>
2022-05-17 15:52:05 +0200

    modif nom P2 + Fin P5

--- P5_Code/front/js/product.js
+++ P5_Code/front/js/product.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(location.search);
 
 const id = params.get("id");
 
-console.table(id);
+// console.table(id);
 
 
 fetch("http://localhost:3000/api/products/"+id)
@@ -15,7 +15,7 @@ fetch("http://localhost:3000/api/products/"+id)
 
     // Ajout de la liste des informations "element" dans la fonction "displayElement" pour les récupérer ensuite
     displayElement(element);
-    console.log(element);
+    // console.log(element);
 
   })
   .catch(function (err) {
@@ -31,33 +31,77 @@ function displayElement(element) {
   img.setAttribute("src", element.imageUrl);
   img.setAttribute("alt", element.altTxt);
 
-  console.log(img);
-
   document.querySelector('.item__img').appendChild(img);
-
-  // let titre = document.getElementsById('title');
-  // titre.textContent = element.name;
 
   document.getElementById('title').innerHTML = element.name;
 
-  // let prix = document.getElementsById('price');
-  // prix.innerHTML = element.price;
+  document.getElementById('description').innerHTML = element.description;
 
   document.getElementById('price').innerHTML = element.price;
 
-  // let description = document.getElementsById('description');
-  // description.textContent = element.description;
 
-  // Classement d'ordre d'affichage parents/enfants
-  // img.appendChild(article);
-  // article.appendChild(image);
-  // article.appendChild(titreH3);
-  // article.appendChild(paragraphe);
-
-  // Classement d'ordre d'affichage de "img" qui sera enfants de l'éléments avec l'id "items"
-  // document.getElementsByClassName("item__img").appendChild(img);
-  // document.getElementById("title").appendChild(img);
+  let list_colors = document.querySelector("#colors");
+  element.colors.forEach(color => {
+    let optionColor = document.createElement("option");
+    optionColor.setAttribute("value", color);
+    optionColor.textContent = color;
+    list_colors.appendChild(optionColor);
+  });
 
   // Table des canapés
-  console.table(element);
+  // console.table(element);
 }
+
+function addToCart(product) {
+  const panier = JSON.parse(localStorage.getItem("panier")) || []
+  if (localStorage.cart) {
+      panier = JSON.parse(localStorage.cart);
+  }
+  //Si le panier du localStorage n'est pas vide
+  if (panier.length) {
+      let modified = false;
+      panier.forEach(element => {
+        if (element.id === product.id && element.color === product.color) {
+            element.quantity += product.quantity;
+            modified = true;
+        }
+      });
+      if (!modified) {
+          panier.push(product);
+      }
+      localStorage.setItem("panier", JSON.stringify(panier));
+    // Si le panier et vide
+  } else {
+      panier.push(product);
+      localStorage.setItem("panier", JSON.stringify(panier));
+  }
+
+}
+
+document.getElementById('addToCart').addEventListener('click',function() {
+
+  // if pour vérifier couleurs bien selectionner
+  const customColor = document.querySelector("#colors").value;
+  const quantity = document.querySelector("#quantity").value;
+  const nameItem = document.getElementById("title").innerHTML;
+
+  if (!customColor) {
+    // si != rien selectionner alert, + return
+    return alert("Veuillez choisir une couleur.")
+  }
+
+  // quantité bien comprise entre 0 et 100
+  if (quantity <= 0 || quantity > 100) {
+    // si != pas pareil alert, + return
+    return alert("Veuillez choisir un nombre entre 1 et 100.")
+  }
+
+  let product = {
+    "id": id,
+    "name": nameItem,
+    "color": customColor,
+    "quantity": Number(quantity),
+  }
+
+  addToCart(product);
+})
--- P5_Code/front/js/script.js
+++ P5_Code/front/js/script.js
@@ -52,5 +52,5 @@ function displayProducts(products) {
     })
   }
   // Table des canapés
-  console.table(products);
+  // console.table(products);
 }
